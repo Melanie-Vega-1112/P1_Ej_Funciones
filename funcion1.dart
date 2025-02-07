53b112dc697bbec0d3fc24e39df6e72223984bc5
Melanie-Vega-1112 <164534346+Melanie-Vega-1112@users.noreply.github.com>
2025-02-07 16:47:42 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+// funcion que no devuelva valor
+//funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+void escribeNombre(){
+  print('Melanie Vega');
+}
+
+void main(){
+ // Melanie Vega Mat: 22308051281112
+ // llamar a la funcion saludar
+ print('Melanie Vega');
+ print('llamando a la funcion saludar');
+    saludar();
+    print('llamando a la uncion escribeNombre');
+    escribeNombre();
+}
