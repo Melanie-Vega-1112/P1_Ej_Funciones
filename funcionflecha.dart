0377921a541a5338b6c5dfdee102403c7cc0d901
Melanie-Vega-1112 <164534346+Melanie-Vega-1112@users.noreply.github.com>
2025-02-10 18:35:36 +0000

    publique funcion flecha

---
+++ funcionflecha.dart
@@ -0,0 +1,25 @@
+int multiplicacion(int a, int b){
+  return a * b;
+}
+int flechamultiplicacion(int a, int b) => a * b;
+
+double divider(int a, int b){
+    return a / b;
+}
+
+double flechadivider(int a, int b) => a / b;
+//returnType functionName(parameters) => expression;
+// funcion principal
+void main(){
+  print('Melanie Vega Mat: 22308051281112');
+  print('llamando a la funcion multiplicacion');
+  print(multiplicacion(120, 5));
+  print(flechamultiplicacion(120, 5));
+  print('llamando a la funcion flechamultiplicacion');
+  print(flechamultiplicacion(120, 5));
+   print('llamando a la funcion divider');
+  print(multiplicacion(120, 5));
+  print(flechamultiplicacion(120, 5));
+  print('llamando a la funcion flechadivider');
+  print(flechamultiplicacion(120, 5));
+}
